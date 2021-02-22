8b839c97fbd8a5eaedfb2d22bb6a941a5314129e
MatheusKael <matheuskael.silva@gmail.com>
2021-02-22 18:36:31 -0400

    Feat: listing providers

--- src/hooks/Auth.tsx
+++ src/hooks/Auth.tsx
@@ -50,6 +50,8 @@ export const AuthProvider: React.FC = ({ children }) => {
       ]);
 
       if (token[1] && user[1]) {
+        api.defaults.headers.authorization = `Bearer ${token[1]}`;
+
         setData({ token: token[1], user: JSON.parse(user[1]) });
       }
       setLoading(false);
@@ -69,6 +71,8 @@ export const AuthProvider: React.FC = ({ children }) => {
       ['@GoBarber:Token', token],
       ['@GoBarber:User', JSON.stringify(user)],
     ]);
+
+    api.defaults.headers.authorization = `Bearer ${token}`;
 
     setData({ token, user });
   }, []);
